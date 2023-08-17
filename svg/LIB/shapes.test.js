dde8e6477cf68a0d9c30a532ffe4ef79d7735901
bronson <disnamedoe@gmail.com>
2023-08-17 16:20:58 -0700

    added logo

---
+++ svg/LIB/shapes.test.js
@@ -0,0 +1,14 @@
+// const 
+//     {Circle, Triangle, Square}= require('./shapes');
+
+// describe('Shape Tests', () => {
+
+//     describe('Circle', () => {
+//         it('should return a circle', () => {
+//             const shape = new Circle('A', 'red', 'white', 'circle');
+
+
+
+
+//     )};
+
