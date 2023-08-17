03fc00ff6676ecf8bb4eb27eaf0505e4222492ec
bronson <disnamedoe@gmail.com>
2023-08-17 16:30:24 -0700

    added shape test

--- svg/LIB/shapes.test.js
+++ svg/LIB/shapes.test.js
@@ -1,14 +1,10 @@
-// const 
-//     {Circle, Triangle, Square}= require('./shapes');
-
-// describe('Shape Tests', () => {
-
-//     describe('Circle', () => {
-//         it('should return a circle', () => {
-//             const shape = new Circle('A', 'red', 'white', 'circle');
-
-
-
-
-//     )};
-
+// using this code to test a triangle
+describe("Triangle test", () => {
+    test("test for a triangle with a #ca00ca background", () => {
+      const shape = new Triangle();
+      shape.setColor("blue");
+      expect(shape.render()).toEqual(
+        '<polygon points="50,10 90,90 10,90" fill="blue" />'
+      );
+    });
+  });
